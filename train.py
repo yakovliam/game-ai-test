2e9d7f8ba5f7dc1a470feef85f8e309af0df8311
yakovliam <jacob@yakovliam.com>
2024-01-01 20:46:08 -0500

    feat: update

---
+++ train.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:62492b4b87b1fd92a2d69a85825780ef472637f7fd972484344a8542d1eec95a
+size 1560
---
+++ validate.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ab6dfe009bdbf5f97c73a82318bac5d668a29e778e56c8f3c121e9837efe1fbc
+size 1588
